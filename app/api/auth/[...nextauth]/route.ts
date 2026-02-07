811ff635071ad08aa75f05faaaa717698cb53dc7
krispinniv-sudo <krispinniv@gmail.com>
2026-02-07 21:27:24 +0200

    fix: Simpler stable NextAuth config

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -21,51 +21,6 @@ export const authOptions = {
         },
     },
     secret: process.env.NEXTAUTH_SECRET,
-    trustHost: true,
-    cookies: {
-        sessionToken: {
-            name: `__Secure-next-auth.session-token`,
-            options: {
-                httpOnly: true,
-                sameSite: 'lax' as const,
-                path: '/',
-                secure: true
-            }
-        },
-        callbackUrl: {
-            name: `__Secure-next-auth.callback-url`,
-            options: {
-                sameSite: 'lax' as const,
-                path: '/',
-                secure: true
-            }
-        },
-        csrfToken: {
-            name: `__Host-next-auth.csrf-token`,
-            options: {
-                sameSite: 'lax' as const,
-                path: '/',
-                secure: true
-            }
-        },
-        pkceCodeVerifier: {
-            name: `__Secure-next-auth.pkce.code_verifier`,
-            options: {
-                sameSite: 'lax' as const,
-                path: '/',
-                secure: true
-            }
-        },
-        state: {
-            name: `__Secure-next-auth.state`,
-            options: {
-                sameSite: 'lax' as const,
-                path: '/',
-                secure: true,
-                maxAge: 900
-            }
-        },
-    }
 }
 
 const handler = NextAuth(authOptions)
